af0df19466366ef145762a859d3786df75f3b01d
hanstar17 <hanstar17@gmail.com>
2019-09-20 15:44:11 -0700

    2 easy problems (bumerang and array add)

---
+++ Leetcode/447_BumerangCount.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/number-of-boomerangs/
+
+#include <vector>
+#include <unordered_map>
+
+inline int distSq(const std::vector<int> &a, const std::vector<int> &b) {
+    const int xd = b[0] - a[0];
+    const int yd = b[1] - a[1];
+    return xd * xd + yd * yd;
+}
+
+inline int countBoomerangs(int n) {
+    return n * (n - 1);
+}
+
+int numberOfBoomerangs(std::vector<std::vector<int>>& points) {
+    const int size = points.size();
+    if (size < 3)
+        return 0;
+
+    int boomerangs = 0;
+    std::unordered_map<int, int> counter;
+    counter.reserve(points.size());
+    for (int i = 0; i < points.size(); ++i) {
+        counter.clear();
+        auto &a = points[i];
+        for (int j = 0; j < points.size(); ++j) {
+            if (i == j)
+                continue;
+            counter[distSq(a, points[j])]++;
+        }
+        for (auto &count : counter)
+            boomerangs += countBoomerangs(count.second);
+    }
+
+    return boomerangs;
+}
+
+#include <iostream>
+
+int main() {
+    std::vector<std::vector<int>> a = {{0,0},{1,0},{2,0}};
+    std::cout << numberOfBoomerangs(a) << std::endl;
+    return 0;
+}
---
+++ Leetcode/989_AddToArray.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/add-to-array-form-of-integer/
+
+#include <vector>
+int CountDigits(int n) {
+    int c = 0;
+    while (n > 0) {
+        n /= 10;
+        ++c;
+    }
+    return c;
+}
+
+std::vector<int> AddToArrayForm(std::vector<int>& A, int K) {
+    const int size = A.size();
+    for (int i = size - 1; K > 0 && i >= 0; --i) {
+        A[i] += K;
+        K = A[i] / 10;
+        A[i] %= 10;
+    }
+
+    int extra = CountDigits(K);
+    if (extra == 0)
+        return std::move(A);
+
+    std::vector<int> B(size + extra);
+    B.reserve(size + extra);
+    B.resize(extra);
+    while (K > 0) {
+        B[--extra] = K % 10;
+        K /= 10;
+    }
+    B.insert(B.end(), A.begin(), A.end());
+    return B;
+}
+
+#include <iostream>
+int main() {
+    std::vector<int> a = {0};
+    auto result = AddToArrayForm(a, 10000);
+    for (auto & a : result) {
+        std::cout << a << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
